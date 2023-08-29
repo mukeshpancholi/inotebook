6c0df7741f3486d35199d3ae299a9cc572dddf76
Mukesh <mk74366@gmail.com>
2023-08-29 22:16:44 +0530

    add console in index

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on Hiiii ${port}`);
 });
